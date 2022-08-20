32d6005cef926c7b6d2d8a1aec88c509e4cedb30
Eugene-Fed <eugene.a.fed@gmail.com>
2022-08-21 01:31:26 +0300

    Привел все настройки к финальной версии

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import os
 from tqdm import tqdm                                           # для отслеживания прогресса выполнения скрипта
 
-SAVING_FRAMES = 10000                                              # сохраняем каждый 15-й кадр
+SAVING_FRAMES = 15                                              # сохраняем каждый 15-й кадр
 WORK_DIRECTORY = "D:\Downloads\ComputerVision_test"
 VIDEO_FILE_NAME = "Camera 3_20220526_003249.mp4"
 XMIN, YMIN = 115, 210
@@ -27,7 +27,7 @@ def main(video_file):
     print("Общее количество кадров в видео: {}".format(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
 
     # цикл по всем кадрам, начиная с 15 (нужно начинать с 14 т.к. индекс 0-based) до конца файла, через каждые 15 кадров
-    for frame_no in range(SAVING_FRAMES, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), SAVING_FRAMES):
+    for frame_no in tqdm(range(SAVING_FRAMES, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), SAVING_FRAMES)):
         # единица в качестве первого параметра - это CV_CAP_PROP_POS_FRAMES
         # или '0-based index of the frame to be decoded/captured next', т.е. метод set(1, int) задает кадр для чтения
         cap.set(1, frame_no);
