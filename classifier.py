63778de3c32db0957f9d4f9cdda478e2d508cbf2
Eugene-Fed <eugene.a.fed@gmail.com>
2022-08-21 13:48:07 +0300

    Подготовлен базовый класс нейронки

--- classifier.py
+++ classifier.py
@@ -1,4 +1,8 @@
 from tqdm import tqdm
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
 import json
 
 # try:
@@ -17,12 +21,32 @@ import json
 WORK_DIRECTORY = "D:\Downloads\ComputerVision_test"
 POSITIVE_SET = "00_hardworkers"
 NEGATIVE_SET = "01_lazyobnes"
+# PyTorch = 'https://pytorch.org/'
+# Help = 'https://neurohive.io/ru/tutorial/glubokoe-obuchenie-s-pytorch/'
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(116 * 116, 1000)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc3 = nn.Linear(1000, 2)
+
+
+def forward(self, x):
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
+    return F.log_softmax(x)
+
 
 def main():
     print('Start classifier')
     # print('Work Dir: {}\n'
     #       'Positive Set: {}\n'
     #       'Negative Set: {}'.format(WORK_DIRECTORY, POSITIVE_SET, NEGATIVE_SET))
+    net = Net()
+    print("{}\n".format(net))
 
 
 if __name__ == '__main__':
